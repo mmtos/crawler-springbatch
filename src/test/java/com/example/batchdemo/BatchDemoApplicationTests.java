c16f795990298e080d465f23fc865f60384d3498
DESKTOP-DMU5SMR\JSH <shjee@metanet.co.kr>
2022-01-06 13:00:22 +0900

    테스트 DB로 H2 사용, Spring Batch 테스트 환경 추가

--- src/test/java/com/example/batchdemo/BatchDemoApplicationTests.java
+++ src/test/java/com/example/batchdemo/BatchDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.batchdemo;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BatchDemoApplicationTests {
+	@Autowired
+	private StringEncryptor jasyptStringEncryptor;
 
 	@Test
 	void contextLoads() {
 	}
+
 
 }
---
+++ src/test/java/com/example/batchdemo/BatchIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.example.batchdemo;
+
+import com.example.batchdemo.config.batch.PersonBatchConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBatchTest
+@SpringBootTest(classes = {PersonBatchConfig.class, TestBatchConfig.class, JobCompletionNotificationListener.class})
+public class BatchIntegrationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @Test
+    public void PERSON_추가_통합테스트() throws Exception{
+        JobExecution je = jobLauncherTestUtils.launchJob();
+        assertThat(je.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
+
+}
